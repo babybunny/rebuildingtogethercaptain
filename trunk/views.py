acd19688cdb3b8221f6c1b9e9c41d9c3af1cc163
babybunny <babybunny@b12e9b45-6f89-5bbe-27c7-5580fe1bb629>
2011-02-15 05:22:48 +0000

    added login button to welcome page, handle users who are not Captain or Staff.

--- trunk/views.py
+++ trunk/views.py
@@ -2,15 +2,12 @@
 
 """Basic views for the entire RTP app."""
 
+import logging
 from google.appengine.api import users
 import django
 from django import http
 from django import shortcuts
 from room import views
-
-def LogOut(request):
-  r = http.HttpResponse('<h1>You have logged out.</h1>')
-  return r
 
 def Welcome(request):
   user = users.GetCurrentUser()
@@ -21,6 +18,7 @@ def Welcome(request):
   # not logged in or no account set up.
   params = {}
   params['user'] = user
+  params['logout_url'] = users.create_logout_url('/')
   return shortcuts.render_to_response('welcome.html', params)
 
 
