aa4b047040c932f2b42ef57fe9ac56b6e1c9a6cd
Luke Stone <babybunny@gmail.com>
2017-04-21 21:08:50 -0700

    staff stuff

--- main.py
+++ main.py
@@ -5,20 +5,32 @@ import os
 import jinja2
 import webapp2
 
-from google.appengine.api import users
+from room import ndb_models
+
+from google.appengine.api import oauth
+
 
 class MainPage(webapp2.RequestHandler):
     """The main UI page, renders the 'index.html' template."""
 
     def get(self):
         """Renders the main page."""
-        template_values = {'show_admin_link': users.IsCurrentUserAdmin()}
+        status = ''
+        try: 
+          user = oauth.get_current_user()
+          status = 'signed in as %s' % user.email()
+        except oauth.Error:
+          status = 'not signed in'
+        template_values = {
+          'status': status,
+        }
         template = jinja_environment.get_template('templates/index.html')
         self.response.out.write(template.render(template_values))
+
 
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 app = webapp2.WSGIApplication([
-    ('/.*', MainPage)
+    ('/', MainPage),
 ], debug=True)
