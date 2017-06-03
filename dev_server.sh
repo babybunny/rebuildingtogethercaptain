962c558ce8e788c176af03ce8dbba3af17e85f49
Luke Stone <babybunny@gmail.com>
2017-06-03 15:59:48 -0700

    added dev startup script

---
+++ dev_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# This starts the dev appserver, clears the datastore, and inserts some test data.
+
+dev_appserver.py --clear_datastore=yes --port 9084 --admin_port 9085 --api_port 9086 app.yaml &
+
+sleep 6
+
+python dev_utilities.py
+
+
+echo "check out the local server at http://localhost:9084"
+
