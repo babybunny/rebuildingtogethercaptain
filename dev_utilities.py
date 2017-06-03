0589db919ce8c66543750e9cc0606c8a5c1e27d7
Luke Stone <babybunny@gmail.com>
2017-06-03 11:58:23 -0700

    added dev_utilities.py

---
+++ dev_utilities.py
@@ -0,0 +1,48 @@
+import argparse
+
+import test_models
+
+try:
+    import dev_appserver
+    dev_appserver.fix_sys_path()
+except ImportError:
+    print('Please make sure the App Engine SDK is in your PYTHONPATH.')
+    raise
+
+from google.appengine.ext import ndb
+from google.appengine.ext.remote_api import remote_api_stub
+
+
+def main(port):
+    servername = 'localhost:{}'.format(port)
+    print servername
+    remote_api_stub.ConfigureRemoteApi(
+        app_id=None,  # don't contact prod server!
+        servername=servername,
+        path='/_ah/remote_api',
+        auth_func=lambda:('', ''),
+        secure=False)
+    
+    # List the first 10 keys in the datastore.
+    keys = ndb.Query().fetch(10, keys_only=True)
+    
+    for key in keys:
+        print(key)
+
+    test_models.CreateAll()
+
+        
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--port', help='localhost port number', default=9086)
+    
+    args = parser.parse_args()
+    
+    main(args.port)
+    
+
+
+
+
