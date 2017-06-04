c2ad7afab3ac6a5c3dd90d3aff98b1004511f3d7
Luke Stone <babybunny@gmail.com>
2017-06-03 17:33:32 -0700

    simplify javascript app

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,15 +1,9 @@
 define(
-    ['app/views/staff'],
-    function(StaffView) {
+    ['backbone'],
+    function(Backbone) {
         return Backbone.Router.extend({
             routes: {
-                'room/staff_home': 'staffHome',
                 '': 'welcome',                
-            },
-            
-            staffHome: function() {
-                console.log('headed to staff home');
-                rooms.views.staff.render()
             },
             welcome: function() {
                 console.log('welcome page');                
