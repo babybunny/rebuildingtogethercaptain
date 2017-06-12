579569a3ef1179731c0ecb421531469482e7f73f
Luke Stone <babybunny@gmail.com>
2017-06-12 09:34:51 -0700

    major cleanup of endpoints stuff

--- app/js/config.js
+++
@@ -1,7 +0,0 @@
-define([], function() {
-    var config = {};
-    config.scopes = 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile';
-    config.clientId = '1093814363166-dp6d5juof62nf0siaja08fabqsh8ber3.apps.googleusercontent.com';
-    return config;
-});
-
--- appengine_config.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-
-ENDPOINTS_PROJECT_DIR = os.path.join(os.path.dirname(__file__),
-                                     'endpoints-proto-datastore')
-sys.path.append(ENDPOINTS_PROJECT_DIR)
