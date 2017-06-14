94a31b601f86dd88c454fb32e45521e7a1ff7f0b
Luke Stone <babybunny@gmail.com>
2017-06-13 23:19:24 -0700

    added tests for Xheader

--- test/test_app.py
+++ test/test_app.py
@@ -14,6 +14,13 @@ class StaffTest(unittest2.TestCase):
 
     def testRoot(self):
         # determines dev user from nose2.cfg
-        # testbed-env = ROOMS_DEV_SIGNIN_EMAIL="rebuildingtogether.staff@gmail.com"
+        # testbed-env = ROOMS_DEV_SIGNIN_EMAIL="rebuildingtogether.nobody@gmail.com"
         response = app.get('/')
-        
+        self.assertEquals('200 OK', response.status)
+        self.assertIn('Welcome to ROOMS', str(response))
+        self.assertIn('rebuildingtogether.nobody@gmail.com', str(response))
+
+    def testRootXHeader(self):
+        response = app.get('/', headers={'X-ROOMS_DEV_SIGNIN_EMAIL': 'rebuildingtogether.staff@gmail.com'})
+        self.assertEquals('200 OK', response.status)
+        self.assertIn('rebuildingtogether.staff@gmail.com', str(response))
