f2c9b5e5898178932c66b0046ff191452c2cccbe
Luke Stone <babybunny@gmail.com>
2017-06-14 00:44:12 -0700

    fixed autocomplete again

--- room/staff.py
+++ room/staff.py
@@ -76,10 +76,10 @@ class AutocompleteHandler(StaffHandler):
     prefix = str(self.request.get('term').lower())
     logging.info(prefix)
     items = self.model_class.query()
-    items.filter(model_class.search_prefixes == prefix)
+    items.filter(self.model_class.search_prefixes == prefix)
     if self.program_filter:
       user, _ = common.GetUser(self.request)
-    items.filter(model_class.program == user.program_selected)
+    items.filter(self.model_class.program == user.program_selected)
     matches = {}
     for i in items.iter():
       label = i.Label()
