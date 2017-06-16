e7c17a8054030a89401a52d2186fe8a68285ffdf
Luke Stone <babybunny@gmail.com>
2017-06-15 23:02:52 -0700

    fix trap

--- dev_server.sh
+++ dev_server.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -x -e
 
-# clean up
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
-
 # This starts the dev appserver, clears the datastore, and inserts some test data.
 
 dev_appserver.py --clear_datastore=yes --port 9084 --admin_port 9085 --api_port 9086 app.yaml &
