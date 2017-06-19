404024a7343d51e3179ba069d2d4b9df8afcb5f1
Luke Stone <babybunny@gmail.com>
2017-06-19 02:09:48 -0700

    better supplier form

--- app/js/views/supplier.js
+++ app/js/views/supplier.js
@@ -4,25 +4,74 @@ define(
         'text!app/templates/simple_form.html'
     ],
     function(SimpleFormView, template) {
-        var fields = [{
-            name: "id", // The key of the model attribute
-            label: "ID", // The label to display next to the control
-            control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
-            disabled: true // By default controls are editable. Here we disabled it.
-        }, {
-            name: "name",
-            label: "Name",
-            control: "input"
-        }, {
-            name: "address",
-            label: "Address",
-            control: "input",
-            extraClasses: ["fancy"],
-            helpMessage: "Full address like 123 Main Street, Big City CA, 95050"
-        }, {
-            control: "button",
-            label: "Save changes"
-        }];
+        var fields = [
+            {
+                name: "id", // The key of the model attribute
+                label: "ID", // The label to display next to the control
+                control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
+                disabled: true // By default controls are editable. Here we disabled it.
+            },
+            // boilerplate
+            {
+                name: "active",
+                label: "Active",
+                control: "select",
+                options: [
+                    {label: "Active", value: "Active"},
+                    {label: "Inactive", value: "Inactive"},
+                ]
+            },
+            {
+                name: "visibility",
+                label: "Visibility",
+                control: "select",
+                options: [
+                    {label: "Everyone", value: "Everyone"},
+                    {label: "Staff Only", value: "Staff Only"},
+                ]
+            },
+            {
+                name: "name",
+                label: "Name",
+                control: "input",
+            },
+            {
+                name: "email",
+                label: "Email",
+                control: "input",
+            },
+            {
+                name: "address",
+                label: "Address",
+                control: "input",
+            },
+            {
+                name: "phone1",
+                label: "Phone1",
+                control: "input",
+            },
+            {
+                name: "phone2",
+                label: "Phone2",
+                control: "input",
+            },
+            {
+                name: "notes",
+                label: "Notes",
+                control: "textarea",
+            },
+            {
+                name: "since",
+                label: "Since",
+                control: "input",
+                disabled: true
+                // "since is a DateProperty('since', auto_now_add=True).  TODO",
+            },
+            {
+                control: "button",
+                label: "Save changes"
+            }
+        ];
         
         var ViewFactory = function(app, loading) {
             return new SimpleFormView(fields, 'supplier', template, app.models.supplier, loading)
