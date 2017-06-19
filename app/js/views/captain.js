2b35bff3e29c2d1b2629e4f1a4191fc19d28e79f
Luke Stone <babybunny@gmail.com>
2017-06-19 00:20:53 -0700

    better captain form

--- app/js/views/captain.js
+++ app/js/views/captain.js
@@ -4,35 +4,69 @@ define(
         'text!app/templates/simple_form.html'
     ],
     function(SimpleFormView, template) {
-        var fields = [{
-            name: "id", // The key of the model attribute
-            label: "ID", // The label to display next to the control
-            control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
-            disabled: true // By default controls are editable. Here we disabled it.
-        }, {
-            name: "name",
-            label: "Name",
-            control: "input"
-        }, {
-            name: "email",
-            label: "Email Address",
-            control: "input",
-            extraClasses: ["fancy"],
-            helpMessage: "used to sign in to ROOMS"
-        }, {
-            name: "notes",
-            label: "Notes",
-            control: "textarea"
-        }, {
-            name: "since",
-            label: "Since",
-            control: "input",
-            disabled: true
-            
-        }, {
-            control: "button",
-            label: "Save changes"
-        }];
+        var fields = [
+            {
+                name: "id", // The key of the model attribute
+                label: "ID", // The label to display next to the control
+                control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
+                disabled: true // By default controls are editable. Here we disabled it.
+            },
+            // boilerplate
+            {
+                name: "rooms_id",
+                label: "Rooms id",
+                control: "input",
+            },
+            {
+                name: "name",
+                label: "Name",
+                control: "input",
+            },
+            {
+                name: "email",
+                label: "Email",
+                control: "input",
+            },
+            {
+                name: "tshirt_size",
+                label: "Tshirt size",
+                control: "input",
+            },
+            {
+                name: "phone_mobile",
+                label: "Phone mobile",
+                control: "input",
+            },
+            {
+                name: "phone_home",
+                label: "Phone home",
+                control: "input",
+            },
+            {
+                name: "phone_fax",
+                label: "Phone fax",
+                control: "input",
+            },
+            {
+                name: "phone_work",
+                label: "Phone work",
+                control: "input",
+            },
+            {
+                name: "phone_other",
+                label: "Phone other",
+                control: "input",
+            },
+            {
+                name: "notes",
+                label: "Notes",
+                control: "textarea",
+            },
+            {
+                control: "button",
+                label: "Save changes"
+            }
+        ];
         
         var ViewFactory = function(app, loading) {
             return new SimpleFormView(fields, 'captain', template, app.models.captain, loading)
