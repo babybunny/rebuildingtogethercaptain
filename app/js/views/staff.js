9c4247cf26b2ad7d779bd7c024e9bf29b61898a2
Luke Stone <babybunny@gmail.com>
2017-06-19 02:06:11 -0700

    better staff form

--- app/js/views/staff.js
+++ app/js/views/staff.js
@@ -4,36 +4,53 @@ define(
         'text!app/templates/simple_form.html'
     ],
     function(SimpleFormView, template) {
-        var fields = [{
-            name: "id", // The key of the model attribute
-            label: "ID", // The label to display next to the control
-            control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
-            disabled: true // By default controls are editable. Here we disabled it.
-        }, {
-            name: "name",
-            label: "Name",
-            control: "input"
-        }, {
-            name: "email",
-            label: "Email Address",
-            control: "input",
-            extraClasses: ["fancy"],
-            helpMessage: "used to sign in to ROOMS"
-        }, {
-            name: "notes",
-            label: "Notes",
-            control: "textarea"
-        }, {
-            name: "since",
-            label: "Since",
-            control: "input",
-            disabled: true
-            
-        }, {
-            control: "button",
-            label: "Save changes"
-        }];
-
+        var fields = [
+            {
+                name: "id", // The key of the model attribute
+                label: "ID", // The label to display next to the control
+                control: "input", // This will be converted to InputControl and instantiated from the proper class under the Backform namespace
+                disabled: true // By default controls are editable. Here we disabled it.
+            },
+            // boilerplate
+            {
+                name: "name",
+                label: "Name",
+                control: "input",
+            },
+            {
+                name: "email",
+                label: "Email",
+                control: "input",
+            },
+            {
+                name: "notes",
+                label: "Notes",
+                control: "textarea",
+            },
+            {
+                name: "program_selected",
+                label: "Program selected",
+                control: "input",
+                disabled: true
+            },
+            {
+                name: "last_welcome",
+                label: "Last welcome",
+                control: "input",
+                disabled: true
+            },
+            {
+                name: "since",
+                label: "Since",
+                control: "input",
+                disabled: true
+                // "since is a DateProperty('since', auto_now_add=True).  TODO",
+            },
+            {
+                control: "button",
+                label: "Save changes"
+            }
+        ];
         
         var ViewFactory = function(app, loading) {
             return new SimpleFormView(fields, 'staff', template, app.models.staff, loading)
