f31876f8e324c970edf02df9ab5645dab87834ab
babybunny <luke@google.com>
2017-06-23 00:00:13 +0100

    fixes js deps

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,8 +13,9 @@ module.exports = function(grunt) {
     grunt.file.mkdir('app/js/lib');
     grunt.file.copy('node_modules/requirejs/require.js', 'build/js/lib/require.js');
     grunt.file.copy('node_modules/requirejs-text/text.js', 'build/js/lib/text.js');
-    grunt.file.copy('node_modules/backbone/backbone-min.js', 'build/js/lib/backbone-min.js');
     grunt.file.copy('node_modules/backbone/backbone.js', 'build/js/lib/backbone.js');
+    grunt.file.copy('node_modules/backbone/backbone-min.js', 'build/js/lib/backbone-min.js');
+    grunt.file.copy('node_modules/underscore/underscore.js', 'build/js/lib/underscore.js');
     grunt.file.copy('node_modules/underscore/underscore-min.js', 'build/js/lib/underscore-min.js');
     grunt.file.copy('node_modules/jquery/dist/jquery.min.js', 'build/js/lib/jquery.js');
     grunt.file.copy('node_modules/jqueryui/jquery-ui.min.js', 'build/js/lib/jquery-ui.js');
