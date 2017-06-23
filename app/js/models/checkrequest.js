381da57873fe6cf1b9703e521d0deb5ba313a3ae
Luke Stone <babybunny@gmail.com>
2017-06-23 22:29:32 +0100

    dont pass empty string for numbers

--- app/js/models/checkrequest.js
+++ app/js/models/checkrequest.js
@@ -1,9 +1,18 @@
 define(
-    ['backbone'],
-    function(Backbone) {
+    ['backbone', 'underscore'],
+    function(Backbone, _) {
         var Model = Backbone.Model.extend({
             // matches first part of method name in @remote.method
             urlRoot: '/wsgi_service.checkrequest_',
+	    validate: function(attrs, options) {
+		var self = this;
+		_.each(['food_amount', 'materials_amount', 'labor_amount'],
+		       function(f) {
+			   if (! self.get(f) ) {
+			       self.set(f, 0);
+			   }
+		       });
+	    }
         });
         
         return Model;
