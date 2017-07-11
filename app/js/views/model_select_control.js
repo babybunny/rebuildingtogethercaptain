afd24420d292d3b1a14604e877261b22722d6485
Luke Stone <babybunny@gmail.com>
2017-07-11 16:40:44 -0700

    reformat

--- app/js/views/model_select_control.js
+++ app/js/views/model_select_control.js
@@ -1,25 +1,28 @@
 define(
     ['backform'],
     function(Backform) {
-	var ModelSelectControl = Backform.SelectControl.extend({
-	    initialize: function() {
-		Backform.SelectControl.prototype.initialize.apply(this, arguments);
-		var self = this;
-		var ModelModule = this.field.get('room_model_module');
-		var modl = new ModelModule();
-		this.field.set('room_model', modl);  // just for debugging
-		modl.on('change', function(m) {		    
-		    var new_options = _.map(m.get('choice'), function(e) { return {label: e.label, value: e.id}; });
-		    new_options.unshift({label: '--- please select one ---', value: null});
-		    self.field.set('options', new_options);
-		    self.render();
-		});
-		modl.fetch();		
-	    },
-	    render: function() {
-		return Backform.SelectControl.prototype.render.apply(this, arguments);
-	    }
-	});
-	return ModelSelectControl;
+	      var ModelSelectControl = Backform.SelectControl.extend({
+	          initialize: function() {
+		            Backform.SelectControl.prototype.initialize.apply(this, arguments);
+		            var self = this;
+		            var ModelModule = this.field.get('room_model_module');
+		            var modl = new ModelModule();
+		            this.field.set('room_model', modl);  // just for debugging
+		            modl.on('change', function(m) {		    
+		                var new_options = _.map(m.get('choice'), function(e) {
+                        return {label: e.label, value: e.id};
+                    });
+		                new_options.unshift({label: '--- please select one ---',
+                                         value: null});
+		                self.field.set('options', new_options);
+		                self.render();
+		            });
+		            modl.fetch();		
+	          },
+	          render: function() {
+		            return Backform.SelectControl.prototype.render.apply(this, arguments);
+	          }
+	      });
+	      return ModelSelectControl;
     }
 );
