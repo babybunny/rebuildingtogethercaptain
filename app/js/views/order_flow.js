0ab1c646fe57d43852f5458bb57e2484b37ac19e
Luke Stone <babybunny@gmail.com>
2017-08-25 22:48:24 -0700

    order_flow: shell of a page for selecting items.

--- app/js/views/order_flow.js
+++ app/js/views/order_flow.js
@@ -4,12 +4,13 @@ define(
         'app/views/rooms_form',
 	      'app/views/model_select_control',
         'app/models/order_form_overview',
+        'app/models/order',
         'text!app/templates/order_choose_form.html',
         'text!app/templates/order_form_button.html',
-        'text!app/templates/order_form_preview.html',
+        'text!app/templates/order_select_items.html',
     ],
-    function(bsdp, RoomFormView, ModelSelectControl, OrderFormOverview,
-             choose_form_template, button_template, preview_template) {
+    function(bsdp, RoomFormView, ModelSelectControl, OrderFormOverview, Order,
+             choose_form_template, button_template, select_items_template) {
         var OrderFlowView = Backbone.View.extend({
             initialize: function(app, loading) {
                 console.log('initializing order_flow');
@@ -17,18 +18,27 @@ define(
                 this.model = this.app.models.order;
                 this.choose_form_template = _.template(choose_form_template);
                 this.order_forms = new OrderFormOverview();
+                this.order = new Order();
                 this.listenTo(this.order_forms, 'add', this.render);
                 this.listenTo(this.model, 'change', this.render);
                 this.order_forms.fetch();
                 this.button_template = _.template(button_template);
-                this.preview_template = _.template(preview_template);
+                this.select_items_template = _.template(select_items_template);
             },
             el: '#order-flow-view',
             render: function() {
                 console.log('render '
                             + ' order_forms ' + this.order_forms
                             + ' model '+ this.model
+                            + ' order '+ this.order
                             + ' app ' + this.app.models.order);
+                if (this.order.has('order_form')) {
+                    console.log('has order form: ' + this.order.get('order_form').get('code'));
+                    var t = this.select_items_template({s: this.model.attributes});
+                    this.$el.html(t);
+                    return this;
+                }
+
                 if (!this.order_forms.models) {
                     console.log('order_flow.render waiting on order_forms');
                     return this;
@@ -39,26 +49,17 @@ define(
                     console.log('need to choose order sheet');
                     var t = this.choose_form_template({s: this.model.attributes});
                     this.$el.html(t);
-                    var previews = this.$('#order-form-previews');
-                    var preview_template = this.preview_template;
                     var buttons = this.$('#order-form-buttons');
                     var button_template = this.button_template;
                     _.each(this.order_forms.models,
                            function(f) {
-                               previews.append(preview_template({sheet: f}));
                                buttons.append(button_template(f.attributes));
                            });
-                    $("div.order_sheet a").hover(function() {
-                        console.log('hover: ' + this.id);
-                        $("div.description").hide();
-                        $("div#preview-" + this.id).show();
-                        $("div#preview-" + this.id).position({
-                            my: "left top",
-                            at: "right top",
-                            of: $(this),
-                            collision: "none",
-                            offset: "30 0",
-                        });
+                    var self = this;
+                    $("#order-form-buttons button").click(function() {
+                        console.log('click: ' + this.id);
+                        self.order.set('order_form', self.order_forms.findWhere({code: this.id}));
+                        self.render();
                     });
 
                 }
