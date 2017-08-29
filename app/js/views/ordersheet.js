9f2dc22f728509c754a87e80916192d2df84c5bc
Kate Mills <kate.mills1124@gmail.com>
2017-08-28 19:13:26 -0700

    added helpMessages to order_form

--- app/js/views/ordersheet.js
+++ app/js/views/ordersheet.js
@@ -18,6 +18,7 @@ define(
                 name: "code",
                 label: "Code",
                 control: "input",
+                helpMessage: "Three-letter code like LUM for Lumber"
             },
             {
                 name: "name",
@@ -38,53 +39,69 @@ define(
                 name: "logistics_instructions",
                 label: "Logistics instructions",
                 control: "textarea",
+                default: "",
+                helpMessage: "Instructions to Captain, appears on logistics form"
             },
+            {
+                name: "supports_extra_name_on_order",
+                label: "Supports extra name on order",
+		            control: "checkbox"
+            },
+             {
+                 control: "spacer"
+             },
             {
                 name: "delivery_options",
                 label: "Delivery options",
-                control: "select",
+                control: "radio",
 		            default: "No",
                 options: [
                     {label: "Yes", value: "Yes"},
                     {label: "No", value: "No"},
                 ],
+                helpMessage: "Allow Captain to select Delivery to site"
+
             },
+
             {
                 name: "retrieval_options",
                 label: "Retrieval options",
-                control: "select",
+                control: "radio",
 		            default: "No",
                 options: [
                     {label: "Yes", value: "Yes"},
                     {label: "No", value: "No"},
-                ]
+                ],
+                helpMessage: "Drop-off and retrieval (like debris box) Note: do not set this with either delivery or pick-up</span>"
             },
-            /* TODO     {
-               name: "supports_extra_name_on_order",
-               label: "Supports extra name on order",
-               // "supports_extra_name_on_order is a BooleanProperty('supports_extra_name_on_order', default=False).  TODO",
-               },
-            */
+
             {
                 name: "pickup_options",
                 label: "Pickup options",
-                control: "select",
+                control: "radio",
 		            default: "No",
                 options: [
                     {label: "Yes", value: "Yes"},
                     {label: "No", value: "No"},
-                ]
+                ],
+                helpMessage: "Allow Captain to select Pick-up from RTP warehouse"
+            },
+            {
+                control: "spacer"
             },
             {
                 name: "default_supplier",
-                label: "Defaultsupplier",
+                label: "Default supplier",
                 control: ModelSelectControl,
 		            room_model_module: SupplierChoices,
+                helpMessage: "Default Supplier, used if Item's supplier is not set."
             },
             {
                 name: "instructions",
                 label: "Instructions",
                 control: "textarea",
+                default: "",
+                helpMessage: "Instructions to Captain, appears on order form"
             },
             {
                 id: "submit",
@@ -92,7 +109,7 @@ define(
                 label: "Save changes"
             }
         ];
-	      
+
         var ViewFactory = function(app, loading) {
             return new RoomFormView({
 		            name: 'ordersheet',
