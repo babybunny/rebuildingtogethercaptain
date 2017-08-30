eeea64df3cb5ce36b347f7a91e4fd54738d6883c
Kate Mills <kate.mills1124@gmail.com>
2017-08-30 15:24:27 -0700

    updated order view

--- app/js/models/order.js
+++ app/js/models/order.js
@@ -4,8 +4,9 @@ define(
         var Model = ProtoModel.extend({
             // matches first part of method name in @remote.method
             urlRoot: '/wsgi_service.order_',
+            must_be_floats: ['sub_total', 'actual_total'],
         });
-        
+
         return Model;
     }
 );
