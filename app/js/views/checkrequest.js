5863869dde4533eabfd43a35f7a9111ce1fd7a10
Kate Mills <kate.mills1124@gmail.com>
2017-08-29 19:35:46 -0700

    updated checkrequest view

--- app/js/views/checkrequest.js
+++ app/js/views/checkrequest.js
@@ -2,7 +2,7 @@ define(
     [
         'bootstrap-datepicker',
         'app/views/rooms_form',
-	      'app/views/model_select_control',
+	    'app/views/model_select_control',
         'app/models/captain_choices',
         'text!app/templates/simple_form.html'
     ],
@@ -12,56 +12,78 @@ define(
                 name: "id",
                 label: "ID",
                 control: "input",
-                disabled: true
+                    disabled: true
             },
-            // boilerplate
             {
+            // ndb.KeyProperty(kind=NewSite)
                 name: "site",
                 label: "Site",
-                control: "input",
-                disabled: true
-            },
-            {
-                name: "state",
-                label: "State",
-                control: "select",
-                options: [
-                    {label: "fulfilled", value: "fulfilled"},
-                    {label: "new", value: "new"},
-                    {label: "deleted", value: "deleted"},
-                    {label: "submitted", value: "submitted"},
-                    {label: "payable", value: "payable"},
-                ]
+                    control: "input",
+                    disabled: true
             },
             {
                 name: "captain",
                 label: "Captain",
-		            control: ModelSelectControl,
-		            room_model_module: CaptainChoices,
+                    control: ModelSelectControl,
+                    room_model_module: CaptainChoices,
+            },
+            {
+                name: "program",
+                label: "Program",
+                control: "input"
             },
             {
                 name: "payment_date",
                 label: "Payment date",
                 control: "datepicker",
                 options: {format: "yyyy-mm-dd"},
+                    default: new Date(),
 		            required: true
+            },
+            {
+                name: "labor_amount",
+                label: "Labor amount",
+                control: "input",
+                    default: 0.0,
+                    helpMessage: "Labor Amount ($)"
+            },
+            {
+                name: "materials_amount",
+                label: "Materials amount",
+                control: "input",
+                    default: 0.0,
+                    helpMessage: "Materials Amount ($)"
+            },
+            {
+                name: "food_amount",
+                label: "Food amount",
+                control: "input",
+                    default: 0.0,
+                    helpMessage: "Food Amount ($)"
+            },
+            {
+                name: "description",
+                label: "Description",
+                control: "textarea"
             },
             {
                 name: "name",
                 label: "Name",
                 control: "input",
-		            required: true
+		            required: true,
+                    helpMessage: "Payable To"
             },
             {
                 name: "address",
                 label: "Address",
                 control: "textarea",
+                    helpMessage: "Payee Address"
             },
             {
                 name: "tax_id",
                 label: "Tax id",
                 control: "input",
-                helpMessage: "we'll notify you if we still need this information to process the check"
+                    helpMessage: "Payee Tax ID, we'll notify you if we still need this information to process the check"
             },
             {
                 name: "form_of_business",
@@ -72,33 +94,32 @@ define(
                     {label: "Sole Proprietor", value: "Sole Proprietor"},
                     {label: "Partnership", value: "Partnership"},
                     {label: "Don't Know", value: "Don't Know"},
-                ]
+                ],
+                    helpMessage: "Payee Business Type"
             },
             {
-                name: "materials_amount",
-                label: "Materials amount",
-                control: "input",
+                name: "state",
+                label: "State",
+                control: "select",
+                options: [
+                    {label: "new", value: "new"},
+                    {label: "submitted", value: "submitted"},
+                    {label: "payable", value: "payable"},
+                    {label: "fulfilled", value: "fulfilled"},
+                    {label: "deleted", value: "deleted"}
+                ],
+                    default: 'new'
             },
-            {
-                name: "labor_amount",
-                label: "Labor amount",
-                control: "input",
-            },
-            {
-                name: "food_amount",
-                label: "Food amount",
-                control: "input",
-            },
-            {
-                name: "description",
-                label: "Description",
-                control: "textarea",
-            },
+            //  This is ndb.UserProperty()
+                //{ namme: "last_editor",
+                //  label: "Last editor",
+                //} control: "input"
             {
                 id: "submit",
                 control: "button",
                 label: "Save changes"
-            }
+            },
+
         ];
 
         var ViewFactory = function(app, loading) {
