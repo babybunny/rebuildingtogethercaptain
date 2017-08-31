6f52183a0137248f7ccfb2016996cd66ef4692e3
Kate Mills <kate.mills1124@gmail.com>
2017-08-30 17:09:17 -0700

    updated site view

--- app/js/models/site.js
+++ app/js/models/site.js
@@ -4,8 +4,16 @@ define(
         var Model = ProtoModel.extend({
             // matches first part of method name in @remote.method
             urlRoot: '/wsgi_service.newsite_',
+            must_be_floats: ['latest_computed_expenses'],
+
+            defaults: {
+                budget: 0,
+                announcement_subject: "Nothing Needs Attention",
+                announcement_body: "Pat yourself on the back - no items need attention.\n"+
+                                            "You have a clean bill of health."
+                }
         });
-        
+
         return Model;
     }
 );
