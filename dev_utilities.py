11b1e61d822506cc4e40e38c9ac6e2746d196a67
Kate Mills <kate.mills1124@gmail.com>
2017-09-19 14:56:59 -0700

    Updated programs for 2018

--- dev_utilities.py
+++ dev_utilities.py
@@ -1,11 +1,11 @@
 import argparse
 
 try:
-    import dev_appserver
-    dev_appserver.fix_sys_path()
+  import dev_appserver
+  dev_appserver.fix_sys_path()
 except ImportError:
-    print('Please make sure the App Engine SDK is in your PYTHONPATH.')
-    raise
+  print('Please make sure the App Engine SDK is in your PYTHONPATH.')
+  raise
 
 from google.appengine.ext import ndb
 from google.appengine.ext.remote_api import remote_api_stub
@@ -14,35 +14,31 @@ from test import test_models
 
 
 def main(port):
-    servername = 'localhost:{}'.format(port)
-    print servername
-    remote_api_stub.ConfigureRemoteApi(
-        app_id=None,  # don't contact prod server!
-        servername=servername,
-        path='/_ah/remote_api',
-        auth_func=lambda:('', ''),
-        secure=False)
-    
-    # List the first 10 keys in the datastore.
-    keys = ndb.Query().fetch(10, keys_only=True)
-    
-    for key in keys:
-        print(key)
+  servername = 'localhost:{}'.format(port)
+  print servername
+  remote_api_stub.ConfigureRemoteApi(
+      app_id=None,  # don't contact prod server!
+      servername=servername,
+      path='/_ah/remote_api',
+      auth_func=lambda: ('', ''),
+      secure=False)
 
-    test_models.CreateAll()
+  # List the first 10 keys in the datastore.
+  keys = ndb.Query().fetch(10, keys_only=True)
 
-        
+  for key in keys:
+    print(key)
+
+  test_models.CreateAll()
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('--port', help='localhost port number, matches API server port in dev_server.sh', default=8082)
-    
-    args = parser.parse_args()
-    
-    main(args.port)
-    
+  parser = argparse.ArgumentParser(
+      description=__doc__,
+      formatter_class=argparse.RawDescriptionHelpFormatter)
+  parser.add_argument(
+      '--port', help='localhost port number, matches API server port in dev_server.sh', default=8082)
 
+  args = parser.parse_args()
 
-
-
+  main(args.port)
