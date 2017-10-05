73dacc5624674cb50632fcec2271f7e9820d785c
Luke Stone <babybunny@gmail.com>
2017-10-05 14:22:05 -0700

    refactored choices API to new file

--- room/wsgi_service.py
+++ room/wsgi_service.py
@@ -22,15 +22,6 @@ class SimpleId(messages.Message):
   id = messages.IntegerField(1, required=True)
 
 
-class Choice(messages.Message):
-  id = messages.IntegerField(1, required=True)
-  label = messages.StringField(2)
-
-
-class Choices(messages.Message):
-  choice = messages.MessageField(Choice, 1, repeated=True)
-
-
 class StaffPosition(messages.Message):
   key = messages.IntegerField(1)
   position_name = messages.StringField(2)
@@ -1342,52 +1333,6 @@ class RoomApi(six.with_metaclass(_GeneratedCruApi, remote.Service)):
       detail = SiteCaptainDetail(sitecaptain=f, name=captain_model.name)
       res.sitecaptain_detail.append(detail)
     return res
-
-  # # # # # # # # # #
-  #     Choices     #
-  # # # # # # # # # #
-  # These APIs exist to populate drop-down selections in UI.
-  # Used for models that have foreign keys.
-
-  @remote.method(message_types.VoidMessage,
-                 Choices)
-  def captain_choices_read(self, request):
-    choices = Choices()
-    for mdl in ndb_models.Captain.query().order(ndb_models.Supplier.name):
-      choices.choice.append(Choice(id=mdl.key.integer_id(), label=mdl.name))
-    return choices
-
-  @remote.method(message_types.VoidMessage,
-                 Choices)
-  def supplier_choices_read(self, request):
-    choices = Choices()
-    for mdl in ndb_models.Supplier.query(ndb_models.Supplier.active == 'Active').order(ndb_models.Supplier.name):
-      choices.choice.append(Choice(id=mdl.key.integer_id(), label=mdl.name))
-    return choices
-
-  @remote.method(message_types.VoidMessage,
-                 Choices)
-  def staffposition_choices_read(self, request):
-    choices = Choices()
-    for mdl in ndb_models.StaffPosition.query().order(ndb_models.StaffPosition.position_name):
-      choices.choice.append(Choice(id=mdl.key.integer_id(), label=mdl.position_name))
-    return choices
-
-  @remote.method(message_types.VoidMessage,
-                 Choices)
-  def jurisdiction_choices_read(self, request):
-    choices = Choices()
-    for mdl in ndb_models.Jurisdiction.query().order(ndb_models.Jurisdiction.name):
-      choices.choice.append(Choice(id=mdl.key.integer_id(), label=mdl.name))
-    return choices
-
-  @remote.method(message_types.VoidMessage,
-                 Choices)
-  def ordersheet_choices_read(self, request):
-    choices = Choices()
-    for mdl in ndb_models.OrderSheet.query().order(ndb_models.OrderSheet.name):
-      choices.choice.append(Choice(id=mdl.key.integer_id(), label=mdl.name))
-    return choices
 
 
 application = service.service_mapping(RoomApi, r'/wsgi_service')
