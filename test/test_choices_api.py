11241c602c88fc9f4803f06bab37c7cffb602464
Luke Stone <babybunny@gmail.com>
2017-10-05 18:31:04 -0700

    test coverage for choices_api

--- test/test_choices_api.py
+++ test/test_choices_api.py
@@ -20,6 +20,62 @@ class ChoicesTest(unittest.TestCase):
   def tearDown(self):
     test_models.DeleteAll(self.keys)
 
+  def testCaptainBadAuth(self):
+    post_json_body = {}
+    response = app.post_json('/choices_api.captain_choices_read',
+                             post_json_body,
+                             status=400,
+                             headers={'x-rooms-dev-signin-email': 'rebuildingtogether.capn@gmail.com'})
+    self.assertEquals('400 Bad Request', response.status)
+
+  def testCaptain(self):
+    post_json_body = {}
+    response = app.post_json('/choices_api.captain_choices_read',
+                             post_json_body,
+                             status=200,
+                             headers={'x-rooms-dev-signin-email': 'rebuildingtogether.staff@gmail.com'})
+    self.assertEquals('200 OK', response.status)
+    self.assertIn(u'choice', response.json)
+    self.assertEquals(1, len(response.json['choice']))
+    self.assertDictContainsSubset({u'label': u'Miss Captain'},
+                                  response.json['choice'][0])
+
+  def testStaffposition(self):
+    post_json_body = {}
+    response = app.post_json('/choices_api.staffposition_choices_read',
+                             post_json_body,
+                             status=200,
+                             headers={'x-rooms-dev-signin-email': 'rebuildingtogether.staff@gmail.com'})
+    self.assertEquals('200 OK', response.status)
+    self.assertIn(u'choice', response.json)
+    self.assertEquals(3, len(response.json['choice']))
+    self.assertDictContainsSubset({u'label': u'position one'},
+                                  response.json['choice'][0])
+
+  def testJurisdiction(self):
+    post_json_body = {}
+    response = app.post_json('/choices_api.jurisdiction_choices_read',
+                             post_json_body,
+                             status=200,
+                             headers={'x-rooms-dev-signin-email': 'rebuildingtogether.staff@gmail.com'})
+    self.assertEquals('200 OK', response.status)
+    self.assertIn(u'choice', response.json)
+    self.assertEquals(2, len(response.json['choice']))
+    self.assertDictContainsSubset({u'label': u'FunkyTown'},
+                                  response.json['choice'][0])
+
+  def testOrdersheet(self):
+    post_json_body = {}
+    response = app.post_json('/choices_api.ordersheet_choices_read',
+                             post_json_body,
+                             status=200,
+                             headers={'x-rooms-dev-signin-email': 'rebuildingtogether.staff@gmail.com'})
+    self.assertEquals('200 OK', response.status)
+    self.assertIn(u'choice', response.json)
+    self.assertEquals(4, len(response.json['choice']))
+    self.assertDictContainsSubset({u'label': u'Debris Box'},
+                                  response.json['choice'][0])
+
   def testSupplier(self):
     post_json_body = {}
     response = app.post_json('/choices_api.supplier_choices_read',
