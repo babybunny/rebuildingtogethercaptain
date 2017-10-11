eebe561180e27ed156128092d72de3d05a186e4e
Luke Stone <babybunny@gmail.com>
2017-10-10 21:23:23 -0700

    comment RequestState

--- gae/room/base_api.py
+++ gae/room/base_api.py
@@ -10,7 +10,9 @@ class BaseApi(remote.Service):
   """Base class for protorpc service WSGI applications implementing APIs."""
 
   # Stash the request state so we can get at the HTTP headers later.
+  # https://github.com/google/protorpc/blob/981174b77c87c222ac38fdc156bd7b2607b31531/protorpc/remote.py#L46
   def initialize_request_state(self, request_state):
+    """request_state: a remote.RequestState instance"""
     self.rs = request_state
 
   def _authorize_staff(self):
