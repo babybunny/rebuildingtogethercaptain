a2bc038469cc2ccf2bea145fcc3b4f0ad497a71b
Luke Stone <babybunny@gmail.com>
2017-12-03 18:52:31 -0800

    make it unfail

--- gae/main.py
+++ gae/main.py
@@ -43,8 +43,8 @@ jinja_environment = jinja2.Environment(
 # Be sure to also configure the /room path with login: required in app.yaml.
 login_required = routes.PathPrefixRoute('/room', [
   webapp2.Route(r'/staff_home',
-                staff.StaffHomeee,
-                name='StaffHomeee'),
+                staff.StaffHome,
+                name='StaffHome'),
   webapp2.Route(r'/staff_home',
                 staff.StaffHome,
                 name='StaffHome'),
