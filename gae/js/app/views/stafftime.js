b5171478a2bd50e36163a3b5b19d2122ab3d4fef
Kate Mills <kate.mills1124@gmail.com>
2017-12-13 16:23:13 -0800

    added .btn-primary to button field

--- gae/js/app/views/stafftime.js
+++ gae/js/app/views/stafftime.js
@@ -81,6 +81,7 @@ define(
             {
                 id: "submit",
                 control: "button",
+                extraClasses: ['btn-primary'],
                 label: "Save changes"
             }
         ];
