d51f5633e412c266e469b37be29cbfcb88d8e6f0
Kate Mills <kate.mills1124@gmail.com>
2018-04-06 15:32:15 -0700

    Added StaffPosition/List request handlers

--- gae/room/staff.py
+++ gae/room/staff.py
@@ -34,9 +34,9 @@ def send_message_with_status(response, message, status=500):
 class SelectProgram(webapp2.RequestHandler):
   """Handler for Staff to select a program.
 
-  This is different from other Staff handlers because it is 
-  the only prerequisite to loading the StaffHome page.  So 
-  it requires that the user is staff but does not require that 
+  This is different from other Staff handlers because it is
+  the only prerequisite to loading the StaffHome page.  So
+  it requires that the user is staff but does not require that
   program is already selected. Bootstrapping.
   """
 
@@ -380,7 +380,7 @@ def _SiteBudgetExportInternal(writable, post_vars):
 def _EntryList(request, model_cls, template, params=None, query=None):
   """Generic helper method to perform a list view.
 
-  This method does not enforce any authorization. It should be called after 
+  This method does not enforce any authorization. It should be called after
   authorization is successful..
 
   Template should iterate over a list called 'entries'.
@@ -877,7 +877,7 @@ class OrderInvoice(StaffHandler):
          'site': order.site.get(),
          }
     return common.Respond(self.request, 'order_invoice', d)
-    
+
 
 def _ChangeOrder(request, order_id, input_sanitizer, output_filter=None):
   """Changes an order field based on POST data from jeditable."""
@@ -1093,3 +1093,17 @@ class RemoveSiteAttachment(blobstore_handlers.BlobstoreDownloadHandler):
 ############################################################################################
 # The above is based on https://cloud.google.com/appengine/docs/standard/python/blobstore/ #
 ############################################################################################
+
+class StaffPositionList(StaffHandler):
+  def get(self):
+    return _EntryList(self.request, ndb_models.StaffPosition, 'staffposition_list')
+
+
+class StaffPosition(EditView):
+
+  searchable_model_class = ndb_models.StaffPosition
+
+  model_class = ndb_models.StaffPosition
+  list_view = 'StaffPositionList'
+  template_value = 'staff position'
+  template_file = 'simple_form'
