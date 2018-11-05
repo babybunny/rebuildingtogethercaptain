f8c9af7b2cf721cbb75f78e1c2b47424dcf81c96
Kate Mills <kate.mills1124@gmail.com>
2018-11-05 14:42:08 -0800

    Added basic staffposition.js model & view

---
+++ gae/js/app/models/staffposition.js
@@ -0,0 +1,11 @@
+define(
+    ['app/models/proto_model'],
+    function(ProtoModel) {
+        var Model = ProtoModel.extend({
+            // matches first part of method name in @remote.method
+            urlRoot: '/cru_api.staffposition_',
+        });
+
+        return Model;
+    }
+);
---
+++ gae/js/app/views/staffposition.js
@@ -0,0 +1,40 @@
+define(
+    [
+        'app/views/rooms_form',
+        'text!app/templates/simple_form.html'
+    ],
+    function(RoomFormView, template) {
+        var fields = [
+            {
+                name: "id",
+                label: "ID",
+                control: "input",
+                disabled: true
+            },
+            {
+                name: "position_name",
+                label: "Position name",
+                control: "input",
+                type: "text",
+                required: true
+            },
+            {
+                id: "submit",
+                control: "button",
+                extraClasses: ['btn-primary'],
+                label: "Save changes"
+            }
+        ];
+
+        var ViewFactory = function(app, loading) {
+            return new RoomFormView({
+                name: 'staffposition',
+                    template: template,
+                    model: app.models.staffposition,
+                    loading: loading,
+                    fields: fields,
+            });
+        }
+        return ViewFactory;
+    }
+)
