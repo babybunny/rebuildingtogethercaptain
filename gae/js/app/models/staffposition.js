e54d87056af976773fb3c2cbc0cd26b62e99ca6f
Kate Mills <kate.mills1124@gmail.com>
2019-01-26 14:12:12 -0800

    simpler validation

--- gae/js/app/models/staffposition.js
+++ gae/js/app/models/staffposition.js
@@ -4,51 +4,18 @@ define(
         var Model = ProtoModel.extend({
             // matches first part of method name in @remote.method
             urlRoot: '/cru_api.staffposition_',
+            must_be_floats: ['hourly_rate', 'mileage_rate'],
 
-            validate: function(attrs){
-                this.errorModel.clear();
+            validate: function(){
+                ProtoModel.prototype.validate.apply(this);
 
-                if(attrs.hourly_rate || attrs.hourly_date){
-                    this.set('hourly_update', {rate: this.get('hourly_rate'), date: this.get('hourly_date')})
-                    if(!attrs.hourly_rate){
-                        this.unset('hourly_update');
-                        this.errorModel.set({hourly_rate: "Please set an Hourly (new rate)."});
-                    }
-                    else if(!attrs.hourly_date){
-                        this.unset('hourly_update');
-                        this.errorModel.set({hourly_date: "Please enter an Hourly (start date)."})
-                    }
-                }
-                if(attrs.mileage_rate || attrs.mileage_date){
-                    this.set('mileage_update', {rate: this.get('mileage_rate'), date: this.get('mileage_date')})
-                    if(!attrs.mileage_rate){
-                        this.unset('mileage_update');
-                        this.errorModel.set({mileage_rate: "Please set a Mileage (new rate)."});
-                    }
-                    else if(!attrs.mileage_date){
-                        this.unset('mileage_update');
-                        this.errorModel.set({mileage_date: "Please enter a Mileage (start date)."})
-                    }
-                }
-                if (!_.isEmpty(_.compact(this.errorModel.toJSON()))) {
-                    return "Validation errors. Please fix.";
-                }
-                else{
-                    this.validModelResponder()
-                }
+                this.attributes.hourly_rates['rate'] = this.get('hourly_rate');
+                this.attributes.mileage_rates['rate'] = this.get('mileage_rate');
+
+                this.set({ hourly_rates: this.attributes.hourly_rates,
+                          mileage_rates: this.attributes.mileage_rates });
             },
-            validModelResponder: function(){
-                if (this.has('hourly_update')){
-                    this.addRate(this.get('hourly_update'), 'hourly_rates');
-                }
-                if (this.has('mileage_update')){
-                    this.addRate(this.get('mileage_update'), 'mileage_rates');
-                }
-            },
-            addRate: function(rate_and_date, rate_name){
-                var rates = this.get(rate_name);
-                this.set(rate_name, rates.concat([rate_and_date]));
-            },
+
             defaults: {
                 hourly_rates: [],
                 mileage_rates: []
