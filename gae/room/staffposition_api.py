9fc2636faaacf0078348432f1f1500dc69dcf663
Kate Mills <kate.mills1124@gmail.com>
2019-04-02 15:37:46 -0700

    code review suggestions addressed

--- gae/room/staffposition_api.py
+++ gae/room/staffposition_api.py
@@ -9,7 +9,7 @@ from protorpc.wsgi import service
 
 import base_api
 import ndb_models
-import protorpc_messages
+from protorpc_messages import SimpleId
 
 package = 'rooms'
 
@@ -28,44 +28,47 @@ class StaffPosition(messages.Message):
 
 class StaffpositionApi(base_api.BaseApi):
 
-  def _staffposition_put(self, request, staffposition=None):
-    if not request.position_name:
+  def staffposition_message_to_model(self, msg, mdl):
+    if not msg.position_name:
       raise remote.ApplicationError('Position name is required')
-    staffposition.position_name = request.position_name
-    staffposition.hourly_rate_after_date = [
-        '{} {:.2f}'.format(r.date, r.rate)
-        for r in request.hourly_rates
-        if r.date and r.rate]
-    staffposition.mileage_rate_after_date = [
-        '{} {:.2f}'.format(r.date, r.rate)
-        for r in request.mileage_rates
-        if r.date and r.rate]
-    staffposition.put()
+    mdl.position_name = msg.position_name
+    mdl.hourly_rate_after_date = [
+      '{} {:.2f}'.format(r.date, r.rate) for r in msg.hourly_rates if r.date and r.rate]
+    mdl.mileage_rate_after_date = [
+      '{} {:.2f}'.format(r.date, r.rate) for r in msg.mileage_rates if r.date and r.rate]
+    return mdl
 
   @remote.method(StaffPosition, message_types.VoidMessage)
   def staffposition_create(self, request):
-    self._authorize_user()
+    self._authorize_staff()
     if request.id:
       raise remote.ApplicationError('must not have id in create')
-    staffposition = ndb_models.StaffPosition()
-    self._staffposition_put(request, staffposition)
+    mdl = ndb_models.StaffPosition()
+    self.staffposition_message_to_model(request, mdl)
+    mdl.put()
     return message_types.VoidMessage()
 
   @remote.method(StaffPosition, message_types.VoidMessage)
   def staffposition_update(self, request):
-    self._authorize_user()
-    if not(request.id):
+    self._authorize_staff()
+    if not request.id:
       raise remote.ApplicationError('id is required to update staffposition')
-    staffposition = ndb.Key(ndb_models.StaffPosition, request.id).get()
-    self._staffposition_put(request, staffposition)
+    mdl = ndb.Key(ndb_models.StaffPosition, request.id).get()
+    if not mdl:
+      raise remote.ApplicationError('No staffposition fournd with key {}'.format(request.id))
+    self.staffposition_message_to_model(request, mdl)
+    mdl.put()
     return message_types.VoidMessage()
 
-  @remote.method(protorpc_messages.SimpleId, StaffPosition)
+  @remote.method(SimpleId, StaffPosition)
   def staffposition_read(self, request):
-    self._authorize_user()
-    if not(request.id):
+    self._authorize_staff()
+    if not request.id:
       raise remote.ApplicationError('id is required to read staffposition')
     mdl = ndb.Key(ndb_models.StaffPosition, request.id).get()
+    if not mdl:
+      raise remote.ApplicationError('No staffposition found with key {}'.format(
+        request.id))
     res = StaffPosition(position_name=mdl.position_name)
     for d, r in (dr.split() for dr in mdl.mileage_rate_after_date):
       res.mileage_rates.append(RateAfterDate(date=d, rate=float(r)))
