5831e2e82af10fff0ac68a15d2006157ee534c73
Luke Stone <babybunny@gmail.com>
2020-04-25 17:45:54 -0700

    reformat

--- gae/js/app/views/model_select_control.js
+++ gae/js/app/views/model_select_control.js
@@ -1,28 +1,28 @@
 define(
     ['backform'],
     function(Backform) {
-	var ModelSelectControl = Backform.SelectControl.extend({
-	    initialize: function() {
-		Backform.SelectControl.prototype.initialize.apply(this, arguments);
-		var self = this;
-		var ModelModule = this.field.get('room_model_module');
-		var modl = new ModelModule({parent: this.model});
-		this.field.set('room_model', modl);  // just for debugging
-		modl.on('change', function(m) {
-		    var new_options = _.map(m.get('choice'), function(e) {
+	      var ModelSelectControl = Backform.SelectControl.extend({
+	          initialize: function() {
+		            Backform.SelectControl.prototype.initialize.apply(this, arguments);
+		            var self = this;
+		            var ModelModule = this.field.get('room_model_module');
+		            var modl = new ModelModule({parent: this.model});
+		            this.field.set('room_model', modl);  // just for debugging
+                modl.on('change', function(m) {
+		                var new_options = _.map(m.get('choice'), function(e) {
                         return {label: e.label, value: e.id};
                     });
-		    new_options.unshift({label: '--- please select one ---',
+		                new_options.unshift({label: '--- please select one ---',
                                          value: null});
-		    self.field.set('options', new_options);
-		    self.render().$el.find('label.control-label:contains("*")').addClass('required');
-		});
-		modl.fetch();
-	    },
-	    render: function() {
-		return Backform.SelectControl.prototype.render.apply(this, arguments);
-	    }
-	});
-	return ModelSelectControl;
+		                self.field.set('options', new_options);
+		                self.render().$el.find('label.control-label:contains("*")').addClass('required');
+		            });
+		            modl.fetch();
+	          },
+	          render: function() {
+		            return Backform.SelectControl.prototype.render.apply(this, arguments);
+	          }
+	      });
+	      return ModelSelectControl;
     }
 );
